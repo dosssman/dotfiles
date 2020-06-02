bb871f2969cd052c5e0f9d87eda8a77dcf836c57
dosssman <dosssman@hotmail.fr>
2020-06-02 13:58:03 +0900

    Removed superfluoes files from PAT configs, add fontconfig for Gotop proper display in Termite

--- desktop_victory/.config/i3/i3lockcustom.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-i3lock -e -c 222222 --screen=1 --clock --ringcolor=666666ff --line-uses-ring --keyhlcolor=2e95b4ff --timecolor=2e95b4ff --ring-width=4.8 --datecolor=2e95b4ff --insidecolor=222222ff 
--- desktop_victory/.config/i3/i3lockcustomblank.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/home/z3r0/.config/i3/i3lockcustom.sh && sleep 1 && xset dpms force standby
