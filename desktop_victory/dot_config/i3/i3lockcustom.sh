a40646977c1a5017470354b09d89bc12f0f6375c
dosssman <dosssman@hotmail.fr>
2021-06-20 00:07:52 +0900

    updated i3lock-color's argument after updated

--- desktop_victory/dot_config/i3/i3lockcustom.sh
+++ desktop_victory/dot_config/i3/i3lockcustom.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-i3lock -e -c 222222 --screen=1 --clock --ringcolor=666666ff --line-uses-ring --keyhlcolor=2e95b4ff --timecolor=2e95b4ff --ring-width=4.8 --datecolor=2e95b4ff --insidecolor=222222ff 
+i3lock -e -c 222222 --screen=1 --clock --ring-color=666666ff --line-uses-ring --keyhl-color=2e95b4ff --time-color=2e95b4ff --ring-width=4.8 --date-color=2e95b4ff --inside-color=222222ff 
